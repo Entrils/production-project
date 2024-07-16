54a6e2b527828852ce3d2d3f4715fbca5014864b
Andrew <106585100+Entrils@users.noreply.github.com>
2024-07-16 19:39:28 +0300

    Routing, Code splitting
    React lazy, suspence

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import { Counter } from './components/Counter'
+import React, { Suspense } from 'react'
 import './index.scss'
+import { Route, Routes } from 'react-router-dom'
+import AboutPage from './pages/AboutPage/AboutPage'
+import MainPage from './pages/MainPage/MainPage'
+import { Link } from 'react-router-dom'
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
+import { MainPageAsync } from './pages/MainPage/MainPage.async'
 
-export const App = () => {
+const App = () => {
   return (
     <div className='app'>
-        sdjhasjdasjdsahdjsa
-        <Counter />
+        <Link to={'/'}>Главная</Link>
+        <Link to={'/about'}>О сайте</Link>
+
+        <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={'/about'} element={<AboutPageAsync/>}/>
+          <Route path={'/'} element={<MainPageAsync/>}/>
+
+        </Routes>
+        </Suspense>
     </div>
   )
 }
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Counter } from './components/Counter';
-import { App } from './App';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-<React.StrictMode>
+    <BrowserRouter>
+    <React.StrictMode>
     <App/>
 </React.StrictMode>
+    </BrowserRouter>
 );
