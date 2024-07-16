1d174a005e2e770da0de46344c5af34b396cc792
Andrew <106585100+Entrils@users.noreply.github.com>
2024-07-16 14:58:47 +0300

    css modules
    miniCssExtractPlugin, css modules(Изоляция стилей)

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import './Counter.scss'
+import classes from './Counter.module.scss'
 
 export const Counter = () => {
     const [ count, setCount] = useState(0);
@@ -8,7 +8,7 @@ export const Counter = () => {
     }
 
   return (
-    <div>
+    <div className={classes.btn}>
         <button onClick={increment}>
             increment
         </button>
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Counter } from './components/Counter';
+import { App } from './App';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
 <React.StrictMode>
-    <Counter/>
+    <App/>
 </React.StrictMode>
 );
