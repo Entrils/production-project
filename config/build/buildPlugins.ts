180db2da71f7bc7c533d6337d8610355eb034040
Andrew <106585100+Entrils@users.noreply.github.com>
2024-07-21 21:16:57 +0300

    Hot module, React Refresh

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -5,7 +5,7 @@ import { BuildOptions } from './types/config';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
-
+    const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
     return [
         new HTMLWebpackPlugin({
             template: paths.html,
@@ -18,6 +18,8 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
         ),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
-        })
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new ReactRefreshWebpackPlugin(),
        ]
 }
