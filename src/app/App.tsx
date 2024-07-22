c08c491456e4af5fb5cce071c8a963c654a57b56
Andrew <106585100+Entrils@users.noreply.github.com>
2024-07-22 20:30:36 +0300

    ESlint, Stylelint

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,10 @@
-import React, { Suspense, useContext, useState } from 'react'
+import React, { Suspense} from 'react'
 import './styles/index.scss'
-import { Route, Routes } from 'react-router-dom'
-import { Link } from 'react-router-dom'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { useTheme } from 'app/providers/ThemeProvider'
-import { AboutPage } from 'pages/AboutPage'
-import { MainPage } from 'pages/MainPage'
 import { AppRouter } from './providers/routers'
 import { Navbar } from 'widgets/Navbar'
 import { Sidebar } from 'widgets/Sidebar'
-import { useTranslation } from 'react-i18next'
 
 const App = () => {
 
